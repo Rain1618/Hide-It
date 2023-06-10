527f4f4290ad2670c6e8493834cdc23068dae9a1
BrendaNamuh <brenda.namuhoranye@mail.mcgill.ca>
2023-06-10 18:44:39 -0400

    Updates posts with infinite scroll + removes HTMLtags from text

---
+++ chrome-ext/scripts/content2.js
@@ -0,0 +1,79 @@
+ 
+   window.addEventListener('load', function() {
+    // Executed on page load
+    data = get_posts();
+    feed_data(data);
+    
+    // Executed on scroll load
+    window.addEventListener('scroll', function() {
+       
+        // Calculate scroll position
+        var scrollPosition = window.innerHeight + window.pageYOffset;
+        var documentHeight = document.documentElement.offsetHeight;
+
+        // Check if the user has reached the bottom of the page
+        if (scrollPosition >= documentHeight) {
+        data = get_posts();
+        feed_data(data);
+        }
+    });
+});
+  
+  function get_posts() {
+    loaded_posts =  document.getElementsByClassName('rpBJOHq2PR60pnwJlUyP0')[0].children;
+    for (var i = 1, len = loaded_posts.length; i < len; i++) {
+       
+        // get title
+        var post = loaded_posts[i].getElementsByTagName('h3')[0].innerHTML;
+        // get text if the post has it
+        paragraphs = loaded_posts[i].getElementsByTagName('p');
+        
+        for (var p = 0, length = paragraphs.length; p<length; p++) {
+
+            // don't include text about why user is seeing the post
+            dont_include = ["Popular near you", "Suggested", "Promoted", 
+                            "Because you visited this community before",
+                            "Popular on Reddit right now", "Videos that redditors liked",
+                            "Some redditors find this funny"];
+            if (!dont_include.includes(paragraphs[p].innerHTML)) {
+                var clean_post = removeHtmlTags(paragraphs[p].innerHTML)
+                post = post.concat(" ", clean_post);
+            }
+        }
+        data.push(post);
+    }
+    console.log(JSON.stringify(data, null, 2));
+    return data
+  }
+  
+  function feed_data(data) {
+
+    // Send a POST request to the Python server
+    fetch('http://localhost:5000/api/submit', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        mode: 'cors',
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(result => {
+        // Process the response from the Python server
+        console.log(result);
+        })
+        .catch(error => {
+        console.error('Error:', error);
+        });
+
+
+    }
+
+    function removeHtmlTags(text) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(text, "text/html");
+        const cleanedText = doc.body.textContent;
+        return cleanedText;
+      }
+
+
