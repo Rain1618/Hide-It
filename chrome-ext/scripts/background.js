2aa9c3ae25fd0b0bb315697e47b8ffb696a375a6
claireguyatt <cg5782@gmail.com>
2023-06-14 14:02:14 -0400

    adding user prefs

--- chrome-ext/scripts/background.js
+++ chrome-ext/scripts/background.js
@@ -8,7 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
         const checkboxes = document.querySelectorAll('input[name="preferences"]:checked');
         const selected_labels = Array.from(checkboxes).map(checkbox => checkbox.parentNode.textContent.trim());
         chrome.storage.sync.set({ triggers: selected_labels }).then(() => {});
-        const chosen_threshold = document.querySelector('threshold').value;
+        const chosen_threshold = parseInt(document.getElementById('threshold').value);
+        console.log(chosen_threshold)
         chrome.storage.sync.set({ threshold: chosen_threshold }).then(() => {});
     })
 });
