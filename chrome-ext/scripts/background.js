607327a97f2a651bf9ed60df95157302c5f6baa1
Rain1618 <115603054+Rain1618@users.noreply.github.com>
2023-06-14 13:38:40 -0400

    Added threshold set by user
    Added

--- chrome-ext/scripts/background.js
+++ chrome-ext/scripts/background.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("preferencesForm");
+    //Determine where the triggers and theshold will show up on the html frontend
     const selectedLabelsElem = document.getElementById("selectedLabels");
+    const selectedThresholdElem = document.getElementById("selectedThreshold");
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
@@ -8,7 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
         // update user preferences
         const checkboxes = document.querySelectorAll('input[name="preferences"]:checked');
         const selectedLabels = Array.from(checkboxes).map(checkbox => checkbox.parentNode.textContent.trim());
+        const userThreshold = document.getElementById("thresholdLevel").value; 
+
         selectedLabelsElem.textContent = "Selected Labels: " + selectedLabels.join(", ");
+        selectedThresholdElem.textContent = "Selected Threshold: " + userThreshold;
+        
         chrome.storage.sync.set({ triggers: selectedLabels }).then(() => {});
 
         var threshold = 'threshold', test_threshold = 0.5;
