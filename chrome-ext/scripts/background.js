7ee705971046ecafa2f4cebd604a4ab989f50441
Rain1618 <115603054+Rain1618@users.noreply.github.com>
2023-06-15 10:14:23 -0400

    Update background.js

--- chrome-ext/scripts/background.js
+++ chrome-ext/scripts/background.js
@@ -1,15 +1,41 @@
-// update user preferences when the user does so from the front end
 document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("preferencesForm");
-    const selectedLabelsElem = document.getElementById("selectedLabels");
+  const form = document.getElementById("preferencesForm");
+  const deletePreferencesBtn = document.getElementById("deletePreferencesBtn");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        const checkboxes = document.querySelectorAll('input[name="preferences"]:checked');
-        const selected_labels = Array.from(checkboxes).map(checkbox => checkbox.parentNode.textContent.trim());
-        chrome.storage.sync.set({ triggers: selected_labels }).then(() => {});
-        const chosen_threshold = parseInt(document.getElementById('threshold').value);
-        console.log(chosen_threshold)
-        chrome.storage.sync.set({ threshold: chosen_threshold }).then(() => {});
-    })
+  //Determines where the triggers and theshold will show up on the html frontend
+  const selectedLabelsElem = document.getElementById("selectedLabels");
+  const selectedThresholdElem = document.getElementById("selectedThreshold");
+
+
+  chrome.storage.local.get("preferences", function(result) {
+    if (result.preferences) {
+      // Preferences exist, display them
+      selectedLabelsElem.textContent = "Selected Labels: " + result.preferences.labels;
+      selectedThresholdElem.textContent = "Selected Threshold: " + result.preferences.threshold;
+    } else {
+      // Preferences don't exist, show the form
+      form.style.display = "block";
+    }
+  });
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const checkboxes = document.querySelectorAll('input[name="preferences"]:checked');
+    const selectedLabels = Array.from(checkboxes).map(checkbox => checkbox.parentNode.textContent.trim()); //Triggers set by user
+
+    const userThreshold = document.getElementById("thresholdLevel").value;
+
+    // Save preferences to storage
+    chrome.storage.local.set({ preferences: { labels: selectedLabels, threshold: userThreshold } });
+
+    selectedLabelsElem.textContent = "Selected Labels: " + selectedLabels.join(", ");
+    selectedThresholdElem.textContent = "Selected Threshold: " + userThreshold;
+
+    //Reload tab after clicking Save Preferences
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.reload(tabs[0].id);
+    });
+  });
 });
+
